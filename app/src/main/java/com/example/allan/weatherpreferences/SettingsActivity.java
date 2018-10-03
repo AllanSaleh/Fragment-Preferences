3bf2918017728e18f6fbf7eb27e131ca29e03060
allan <allansaleh95@hotmail.com>
2018-10-03 20:29:53 +0300

    Innitial Commit

---
+++ app/src/main/java/com/example/allan/weatherpreferences/SettingsActivity.java
@@ -0,0 +1,40 @@
+package com.example.allan.weatherpreferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public static class WeatherPreferenceFragment extends PreferenceFragment{
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            //Preference location = findPreference(getString(R.string.shared_preferences_key));
+            
+
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
